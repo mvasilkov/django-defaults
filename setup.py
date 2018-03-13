e25c6d55c591318f92778262c7122866d9c9fdec
Mark Vasilkov <mvasilkov@gmail.com>
2018-03-13 13:45:04 +0200

    Update README

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 if __name__ == '__main__':
     setup(name='django-defaults',
 
-          version='0.0.0',
+          version='0.0.1',
 
           description='Django settings sans boilerplate',
-          long_description='Django settings sans boilerplate',
+          long_description=open('README.md', encoding='utf-8').read(),
 
           url='https://github.com/mvasilkov/django-defaults',
 
